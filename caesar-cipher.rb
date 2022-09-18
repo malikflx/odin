bebc01f1d3b05e26481dbb30b786b472563b9849
Malik Felix <malikfelix50@gmail.com>
2022-09-17 23:40:51 -0400

    WIP caesar-cipher

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -10,3 +10,20 @@
 # Don’t forget to keep the same case.
 
 # Solution
+def caesar_cipher(string)
+  shift = 5
+  converted_string = string.downcase.bytes.map{|a| a - 96 + shift}
+  encrypted_message = converted_string.join
+  decrypted_message = Array(encrypted_message)
+  new_arr = []
+  something = encrypted_message.split('')
+  for i in something
+    char_code = something[i].ord
+    new_arr.push(char_code)
+  end
+  puts new_arr
+
+  decrypted_message
+end
+
+puts caesar_cipher("abcdefg")
